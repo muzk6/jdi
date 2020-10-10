71da2acedb069540408281789c640691c13b6ef8
muzk6 <muzk6x@gmail.com>
2020-10-10 14:23:07 +0800

    fix. #773e2ffe90c9905718e00f894d4e97e765cfb6fc

--- tests/init.php
+++ tests/init.php
@@ -9,6 +9,3 @@ require $path_root . '/vendor/autoload.php';
 App::init([
     'config.app_env' => 'dev', // 当前环境；dev 时回显错误信息
 ]);
-
-$path_root = realpath(__DIR__); // 项目根目录
-require $path_root . '/vendor/autoload.php';
