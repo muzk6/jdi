f77e1c4557c8bb1d658b1ab2fe1f0a30d48cce8b
muzk6 <muzk6x@gmail.com>
2025-01-24 15:22:17 +0800

    优化例子 demo.blade.php, 去掉依赖的 jquery

--- views/demo.blade.php
+++ views/demo.blade.php
@@ -11,7 +11,7 @@
     <input type="text" name="first_name" value="{{ $first_name }}">
     <input type="text" name="last_name" value="{{ $last_name }}">
     <button>Doc Submit</button>
-    <input type="button" id="ok" value="XHR Submit"/>
+    <input type="button" onclick="xhr()" value="XHR Submit"/>
 </form>
 <label style="display: block; margin-top: 30px;">{{ $user_id ? "ID: {$user_id} 已登录" : '未登录' }}</label>
 <form style="display: inline-block" method="post" action="/demo/login">
@@ -23,27 +23,38 @@
     {!! csrf_field() !!}
     <button>Logout</button>
 </form>
-<script src="https://cdn.bootcss.com/jquery/3.4.1/jquery.min.js"></script>
 <script>
-    @if (flash_has('msg'))
-    alert('{{ flash_get('msg') }}');
-    @endif
+@if (flash_has('msg'))
+alert('{{ flash_get('msg') }}');
+@endif
 
-    @if (flash_has('data'))
-    alert('{!! json_encode(flash_get('data')) !!}');
-    @endif
+@if (flash_has('data'))
+alert('{!! json_encode(flash_get('data')) !!}');
+@endif
 
-    $(function () {
-        $('#ok').on('click', function () {
-            let data = {};
-            $('form input[name]').each(function () {
-                data[$(this).attr('name')] = $(this).val();
-            });
-            $.post('/demo/xhr', data, function (data) {
-                alert(JSON.stringify(data));
-            }, 'json');
+function xhr() {
+    let data = {};
+    document.querySelectorAll('form input[name]').forEach(function (input) {
+        data[input.getAttribute('name')] = input.value;
+    });
+
+    fetch('/demo/xhr', {
+        method: 'post',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+        },
+        body: new URLSearchParams(data).toString(),
+    }).then(resp => {
+        if (!resp.ok) {
+            return
+        }
+
+        resp.json().then(data => {
+            alert(JSON.stringify(data));
         });
     });
+}
 </script>
 </body>
 </html>
